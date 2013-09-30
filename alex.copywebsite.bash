45f37ae2c51e6a4f544b8395d4a68b9c4310f09b
megazear7 <alexlockhart7@gmail.com>
2013-09-30 09:33:38 -0400

    edited alex.copywebsite.bash to be updated with new file organization

--- alex.copywebsite.bash
+++ alex.copywebsite.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo cp jjj/json-example.js /var/www/
-sudo cp jjj/test.html /var/www/
-sudo cp jjj/response.cgi /usr/lib/cgi-bin/
+sudo cp demoframework/json-example.js /var/www/
+sudo cp demoframework/test.html /var/www/
+sudo cp demoframework/response.cgi /usr/lib/cgi-bin/
 sudo cp -r characterClient /var/www/
 

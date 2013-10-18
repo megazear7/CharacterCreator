458592bdd35d3534a74565a3a4cddecaa83e83b0
megazear7 <alexlockhart7@gmail.com>
2013-10-18 10:06:43 -0400

    added comments to ales.copywebsite.bash

--- alex.copywebsite.bash
+++ alex.copywebsite.bash
@@ -5,3 +5,8 @@ sudo cp characterClient/response.cgi /usr/lib/cgi-bin/
 sudo cp -r characterClient /var/www/
 sudo cp -r lib /var/www/
 
+# on alex's computer the apache directories are as follows:
+# /var/www/                       for html files
+# /usr/lib/cgi-bin/   		      for cgi files
+# /var/log/apache2/error.log      for error log
+# /var/log/apache2/access.log     for access log
